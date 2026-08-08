57fdccff266bb300a7a2bc9ea41fc27eb4cc3cd6
nickshubspot <nicks.hubspot@gmail.com>
2026-08-08 23:01:01 +0530

    Update Supabase URL to remove '/rest/v1/'

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -15,7 +15,7 @@
 (function () {
   'use strict';
 
-  const SUPABASE_URL = 'https://lumvxwwbfoucfckycdal.supabase.co/rest/v1/';
+  const SUPABASE_URL = 'https://lumvxwwbfoucfckycdal.supabase.co';
   const SUPABASE_ANON_KEY = 'sb_publishable_Lbfszemvg7eE8ieBfwzlJA_CKjHI_9k';
 
   const MAX_LENGTHS = {
