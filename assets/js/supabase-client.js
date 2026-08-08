e4ff5d1e083f4b7e415a50f9cd0acda04ffa85bb
nickshubspot <nicks.hubspot@gmail.com>
2026-08-08 14:15:34 +0530

    Update Supabase URL and anon key

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -15,8 +15,8 @@
 (function () {
   'use strict';
 
-  const SUPABASE_URL = 'https://placeholder-project.supabase.co';
-  const SUPABASE_ANON_KEY = 'placeholder-anon-key';
+  const SUPABASE_URL = 'https://lumvxwwbfoucfckycdal.supabase.co/rest/v1/';
+  const SUPABASE_ANON_KEY = 'sb_publishable_Lbfszemvg7eE8ieBfwzlJA_CKjHI_9k';
 
   const MAX_LENGTHS = {
     fullName: 200,
